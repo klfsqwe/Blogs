3e57a2866962534ed3ca4e9433344549240e88db
klfsqwe <69022640+klfsqwe@users.noreply.github.com>
2025-12-25 20:58:39 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://blogs.ssfrg.com', // 你的最终域名
-  // 开启 View Transitions
-  experimental: {
-    viewTransitions: true,
-  },
+  site: 'https://blogs.ssfrg.com',
 });
